163d4b29c9be7988156a84996373663cedac3844
hjjulian <hjjulian@myseneca.ca>
2024-07-27 01:33:17 -0400

    Initial commit

--- modules/collegeData.js
+++ modules/collegeData.js
@@ -1,6 +1,7 @@
 // Import the fs module to handle file system operations.
 const fs = require('fs');
 const path = require('path');
+
 // Define the Data class to structure the student and course data.
 class Data {
     constructor(students, courses) {
@@ -12,50 +13,60 @@ class Data {
 // Declare a variable to hold the data collection once it's initialized.
 let dataCollection = null;
 
+// Paths to JSON files. Using /tmp for Vercel's writable directory for students.
+const studentsPath = path.join('/tmp', 'students.json');
+const coursesPath = path.join(__dirname, '../data', 'courses.json');
+
 // Function to initialize the data collection by reading and parsing JSON files.
 function initialize() {
     return new Promise((resolve, reject) => {
-        const studentsPath = path.join(__dirname, '../data', 'students.json');
-        const coursesPath = path.join(__dirname, '../data', 'courses.json');
-
-        // Read the students.json file.
-        fs.readFile(studentsPath, 'utf8', (err, studentDataFromFile) => {
-            if (err) {
-                // If there's an error reading the file, reject the promise.
-                return reject("unable to read students.json");
-            }
-
-            let students;
-            try {
-                // Try to parse the student data.
-                students = JSON.parse(studentDataFromFile);
-            } catch (parseErr) {
-                // If parsing fails, reject the promise.
-                return reject("unable to parse students.json");
-            }
-
-            // Read the courses.json file.
-            fs.readFile(coursesPath, 'utf8', (err, courseDataFromFile) => {
-                if (err) {
-                    // If there's an error reading the file, reject the promise.
-                    return reject("unable to read courses.json");
-                }
-
-                let courses;
-                try {
-                    // Try to parse the course data.
-                    courses = JSON.parse(courseDataFromFile);
-                } catch (parseErr) {
-                    // If parsing fails, reject the promise.
-                    return reject("unable to parse courses.json");
-                }
-
-                // Once both files are successfully read and parsed, create a new Data object.
-                dataCollection = new Data(students, courses);
-                // Resolve the promise to indicate successful initialization.
-                resolve();
+        // Check if students.json exists in /tmp. If not, copy from initial location.
+        if (!fs.existsSync(studentsPath)) {
+            fs.copyFile(path.join(__dirname, '../data', 'students.json'), studentsPath, (err) => {
+                if (err) return reject("Error copying students.json file");
+                readDataFiles();
             });
-        });
+        } else {
+            readDataFiles();
+        }
+
+        // Function to read data files and set up the data collection.
+        function readDataFiles() {
+            // Read the students.json file.
+            fs.readFile(studentsPath, 'utf8', (err, studentDataFromFile) => {
+                if (err) {
+                    return reject("unable to read students.json");
+                }
+
+                let students;
+                try {
+                    // Try to parse the student data.
+                    students = JSON.parse(studentDataFromFile);
+                } catch (parseErr) {
+                    return reject("unable to parse students.json");
+                }
+
+                // Read the courses.json file.
+                fs.readFile(coursesPath, 'utf8', (err, courseDataFromFile) => {
+                    if (err) {
+                        return reject("unable to read courses.json");
+                    }
+
+                    let courses;
+                    try {
+                        // Try to parse the course data.
+                        courses = JSON.parse(courseDataFromFile);
+                    } catch (parseErr) {
+                        return reject("unable to parse courses.json");
+                    }
+
+                    // Once both files are successfully read and parsed, create a new Data object.
+                    dataCollection = new Data(students, courses);
+                    // Resolve the promise to indicate successful initialization.
+                    resolve();
+                });
+            });
+        }
     });
 }
 
@@ -63,10 +74,8 @@ function initialize() {
 function getAllStudents() {
     return new Promise((resolve, reject) => {
         if (dataCollection && dataCollection.students.length > 0) {
-            // If students exist in the data collection, resolve the promise with the student data.
             resolve(dataCollection.students);
         } else {
-            // If no students are found, reject the promise.
             reject("no results returned");
         }
     });
@@ -76,31 +85,24 @@ function getAllStudents() {
 function getCourses() {
     return new Promise((resolve, reject) => {
         if (dataCollection && dataCollection.courses.length > 0) {
-            // If courses exist in the data collection, resolve the promise with the course data.
             resolve(dataCollection.courses);
         } else {
-            // If no courses are found, reject the promise.
             reject("no results returned");
         }
     });
 }
 
-
 // Function to get students by course from the data collection.
 function getStudentsByCourse(course) {
     return new Promise((resolve, reject) => {
         if (dataCollection) {
-            // Filter the students to find those who belong to the specified course.
             const studentsByCourse = dataCollection.students.filter(student => student.course == course);
             if (studentsByCourse.length > 0) {
-                // If students are found, resolve the promise with the student data.
                 resolve(studentsByCourse);
             } else {
-                // If no students are found, reject the promise.
                 reject("no results returned");
             }
         } else {
-            // If dataCollection is not initialized, reject the promise.
             reject("no results returned");
         }
     });
@@ -110,17 +112,13 @@ function getStudentsByCourse(course) {
 function getStudentByNum(num) {
     return new Promise((resolve, reject) => {
         if (dataCollection) {
-            // Find the student whose studentNum matches the provided number.
             const student = dataCollection.students.find(student => student.studentNum == num);
             if (student) {
-                // If a student is found, resolve the promise with the student data.
                 resolve(student);
             } else {
-                // If no student is found, reject the promise.
                 reject("no results returned");
             }
         } else {
-            // If dataCollection is not initialized, reject the promise.
             reject("no results returned");
         }
     });
@@ -129,17 +127,12 @@ function getStudentByNum(num) {
 // Add student function
 function addStudent(studentData) {
     return new Promise((resolve, reject) => {
-        // Set TA to true or false based on the checkbox
         studentData.TA = studentData.TA ? true : false;
-        
-        // Set studentNum
         studentData.studentNum = dataCollection.students.length + 1;
-        
-        // Add the new student to the array
         dataCollection.students.push(studentData);
 
-        // Save the updated array to the JSON file
-        fs.writeFile('./data/students.json', JSON.stringify(dataCollection.students, null, 2), (err) => {
+        // Save the updated array to the JSON file in /tmp
+        fs.writeFile(studentsPath, JSON.stringify(dataCollection.students, null, 2), (err) => {
             if (err) {
                 reject("Error writing to students.json file");
             } else {
@@ -153,38 +146,32 @@ function addStudent(studentData) {
 function getCourseById(id) {
     return new Promise((resolve, reject) => {
         if (dataCollection) {
-            // Find the course whose courseId matches the provided id.
             const course = dataCollection.courses.find(course => course.courseId == id);
             if (course) {
-                // If a course is found, resolve the promise with the course data.
                 resolve(course);
             } else {
-                // If no course is found, reject the promise.
                 reject("query returned 0 results");
             }
         } else {
-            // If dataCollection is not initialized, reject the promise.
             reject("no results returned");
         }
     });
 }
 
-// This function finds the student in the students array by studentNum
-
+// Update student function
 function updateStudent(studentData) {
     return new Promise((resolve, reject) => {
         let index = dataCollection.students.findIndex(student => student.studentNum == studentData.studentNum);
 
         if (index !== -1) {
-            // Update the student record with new data
             dataCollection.students[index] = {
                 ...dataCollection.students[index],
                 ...studentData,
                 TA: studentData.TA === 'on' // Convert 'on' to true, else false
             };
 
-            // Write the updated data back to the JSON file
-            fs.writeFile(path.join(__dirname, '../data', 'students.json'), JSON.stringify(dataCollection.students, null, 2), (err) => {
+            // Write the updated data back to the JSON file in /tmp
+            fs.writeFile(studentsPath, JSON.stringify(dataCollection.students, null, 2), (err) => {
                 if (err) {
                     console.error("Error writing to students.json file:", err);
                     return reject("Error writing to students.json file");
@@ -198,9 +185,7 @@ function updateStudent(studentData) {
     });
 }
 
-
-
-// Export the new function along with the others.
+// Export the functions
 module.exports = {
     initialize,
     getAllStudents,
